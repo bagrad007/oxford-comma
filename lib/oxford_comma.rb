a3b144ef48c618f83886025abe74680a0d9d51b2
Luke Gill <bagrad007@gmail.com>
2021-01-13 15:53:05 -0500

    commit tree 88901f4516c37688ece098dc03cae229b719eef4

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
+require 'pry'
 def oxford_comma(array)
+    array_length = array.length
+    case array_length
+    when 1
+        array.join
+    when 2
+       "#{ array[0]} and #{array[1]}"
+    else
+        array[-1].insert(0, "and ")
+        array.join(", ")
+        
+        # "#{array.join} #{array.index(-1), and #{array.last}"
 
+    end
+    
+    # binding.pry
 end
